f980084324d9cc666933e0132cdc358a29f2e7f6
KeigoKusano <keigo.kusano.9v@stu.hosei.ac.jp>
2024-01-30 23:16:53 +0900

    8-5-4_edit_error

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -39,7 +39,7 @@ class PostController extends Controller
     {
         $input_post = $request['post'];
         $post->fill($input_post)->save();
-
+        
         return redirect('/posts/' . $post->id);
     }
 }
