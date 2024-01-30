7f8c1d36c62f01f7f0acc72dd35028342962fc6c
KeigoKusano <keigo.kusano.9v@stu.hosei.ac.jp>
2024-01-30 11:43:17 +0900

    8-4-2_blog_create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,4 +13,14 @@ class PostController extends Controller
         //return view('posts.index')->with(['posts' => $post->getByLimit(1)]);
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit(2)]);
     }
+    public function show(Post $post)
+    {
+        //return view('posts.show')->with(['post' => $post]);
+        //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+        return view('posts.show')->with(['post' => $post]);
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use App\Http\Controllers\PostController;  //外部にあるPostControllerクラ�
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', [PostController::class, 'index']);   
+Route::get('/', [PostController::class, 'index']);  
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('posts/{post}', [PostController::class ,'show']);
 /*:Route::get('/', function () {
     return view('posts/index');
 });*/
