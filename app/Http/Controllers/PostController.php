d528e8995e2962985867d10a83b24ae267a0d219
KeigoKusano <keigo.kusano.9v@stu.hosei.ac.jp>
2024-02-03 21:37:14 +0900

    kariqram_last_commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,24 +5,56 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Http\Requests\PostRequest; // useする
+use App\Models\Category;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
+        // クライアントインスタンス生成
+        $client = new \GuzzleHttp\Client();
+
+        // GET通信するURL
+        $url = 'https://teratail.com/api/v1/questions';
+
+        // リクエスト送信と返却データの取得
+        // Bearerトークンにアクセストークンを指定して認証を行う
+        $response = $client->request(
+            'GET',
+            $url,
+            ['Bearer' => config('services.teratail.token')]
+        );
+        
+        // API通信で取得したデータはjson形式なので
+        // PHPファイルに対応した連想配列にデコードする
+        $questions = json_decode($response->getBody(), true);
+        
+        // index bladeに取得したデータを渡す
+        return view('posts.index')->with([
+            'posts' => $post->getPaginateByLimit(),
+            'questions' => $questions['questions'],
+        ]);
+    }
+    //public function index(Post $post)
+    //{
         //return $post->get();
         //return view('posts.index')->with(['posts' => $post->getByLimit(1)]);
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
-    }
+        //return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);
+    //}
+    
     public function show(Post $post)
     {
         //return view('posts.show')->with(['post' => $post]);
         //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
         return view('posts.show')->with(['post' => $post]);
     }
-    public function create()
+    //public function create()
+    //{
+        //return view('posts.create');
+    //}
+    public function create(Category $category)
     {
-        return view('posts.create');
+        return view('posts.create')->with(['categories' => $category->get()]);
     }
     public function store(PostRequest $request, Post $post)
     {
